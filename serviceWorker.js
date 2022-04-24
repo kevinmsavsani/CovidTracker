1f42f44a1ecfb26674f4b12252267f76fc45e544
kevinmsavsani <kevinmsavsani@gmail.com>
2022-04-24 17:12:59 +0530

    updated service worker

--- serviceWorker.js
+++ serviceWorker.js
@@ -4,13 +4,7 @@
   'use strict';
 
   var staticFilesToCache = [
-    '.',
-    'index.html',
-    '/dist/app.bundle.js',
     '/offline.html',
-    '/src/assets/covid19.png',
-    'https://fonts.googleapis.com/css?family=Roboto:300,400,500',
-    'https://fonts.gstatic.com/s/roboto/v16/RxZJdnzeo3R5zSexge8UUVtXRa8TVwTICgirnJhmVJw.woff2'
   ];
 
   var apiToCache = [
@@ -78,26 +72,28 @@
   });
 
   self.addEventListener('fetch', function (event) {
-    const cached = caches.match(event.request);
-    const fetched = fetch(event.request);
-    const fetchedCopy = fetched.then(resp => resp.clone());
-
-    // Call respondWith() with whatever we get first.
-    // If the fetch fails (e.g disconnected), wait for the cache.
-    // If there’s nothing in cache, wait for the fetch. 
-    // If neither yields a response, return a 404.
-    event.respondWith(
-      Promise.race([fetched.catch(_ => cached), cached])
-        .then(resp => resp || fetched)
-        .catch(_ => new Response(null, { status: 404 }))
-    );
-
-    // Update the cache with the version we fetched
-    event.waitUntil(
-      Promise.all([fetchedCopy, caches.open('cache-v1')])
-        .then(([response, cache]) => cache.put(event.request, response))
-        .catch(_ => {/* eat any errors */ })
-    );
+    if(event.request.url.includes("api")) {
+      const cached = caches.match(event.request);
+      const fetched = fetch(event.request);
+      const fetchedCopy = fetched.then(resp => resp.clone());
+      // Call respondWith() with whatever we get first.
+      // If the fetch fails (e.g disconnected), wait for the cache.
+      // If there’s nothing in cache, wait for the fetch. 
+      // If neither yields a response, return a 404.
+      event.respondWith(
+        Promise.race([fetched.catch(_ => cached), cached])
+          .then(resp => resp || fetched)
+          .catch(_ => {return caches.match('/offline.html');})
+      );
+  
+      // Update the cache with the version we fetched
+      event.waitUntil(
+        Promise.all([fetchedCopy, caches.open(apiCaches)])
+          .then(([response, cache]) => cache.put(event.request, response))
+          .catch(_ => {return caches.match('/offline.html'); })
+      );
+    }
+    
   });
 
 })();
